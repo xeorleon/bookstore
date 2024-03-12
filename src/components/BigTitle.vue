3a16a98e7139a5fe893ba307a8beffc50f5ea232
xeorleon <xeorleon@gmail.com>
2024-03-12 06:59:00 +0300

    Added bigtitle component

---
+++ src/components/BigTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="big-head">
+    <h2 class="big-title" :class="[size]">{{ title }}</h2>
+    <div class="big-line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Default Title",
+      require: true,
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+  },
+};
+</script>
+
+<style></style>
