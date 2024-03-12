8e8a51a3211b821eefad7a4e64fa4cde6e580832
xeorleon <xeorleon@gmail.com>
2024-03-12 03:48:52 +0300

    Axios configurations have been set up  and prepared for use as a helper.

---
+++ src/helpers/axios.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import Vue from "vue";
+const client = axios.create();
+
+
+client.defaults.baseURL = "https://www.googleapis.com/books/v1/"
+
+
+client.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    console.log(config)
+    config.url = config.url + '?key=AIzaSyBv7dXolJMozxbE8TK5jMS5m18F8c8wE8A'
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+Vue.prototype.$client = client;
+export { client };
